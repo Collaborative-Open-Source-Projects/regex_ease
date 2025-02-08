34ad8b71b94ae61af41cf9c9479dda53b9d799a6
siddeshshewde <siddesh.darkeyed95@gmail.com>
2025-02-08 10:49:33 -0500

    Feature for Valid URL

--- regex_ease/patterns.py
+++ regex_ease/patterns.py
@@ -3,12 +3,15 @@ import re
 class Patterns:
     """Common regex patterns for validation."""
 
+    # Email Pattern
     EMAIL_PATTERN = re.compile(
         r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
     )
 
-    @staticmethod
-    def email(email: str) -> bool:
-        """Validates an email address."""
-
-        return bool(Patterns.EMAIL_PATTERN.fullmatch(email))
+    # URL Pattern
+    URL_PATTERN = re.compile(
+        r"^(https?|ftp):\/\/"  # Protocol (http, https, ftp)
+        r"([a-zA-Z0-9.-]+(\.[a-zA-Z]{2,})+)"  # Domain
+        r"(:\d{2,5})?"  # Optional Port
+        r"(\/[^\s]*)?$"  # Path
+    )
